80606b694e8f5dc80dee92be38a388eb19ed239e
Aqeelhusny <aqeelhusny@gmail.com>
2025-07-21 16:10:27 +0530

    text center

--- 404.php
+++ 404.php
@@ -14,7 +14,7 @@ get_header();
 
 		<section class="error-404 not-found">
 			<header class="page-header">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'excellink-store' ); ?></h1>
+				<h1 class="page-title text-center"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'excellink-store' ); ?></h1>
 			</header><!-- .page-header -->
 		</section><!-- .error-404 -->
 
